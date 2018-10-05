7795f10bef23da1dce060aeb0fcd5895bca8719f
Anton Chusovitin <antonion2011@yandex.ru>
2018-10-05 23:57:30 +0700

    Добавил SQL запросы для * проверки существования пользователя * Выбор данных пользователя * Выбор данных о компании

---
+++ api/config.py
@@ -0,0 +1,12 @@
+HEADER = {'Access-Control-Allow-Origin': '*'}
+DATABASE = {
+    "dbname": "ARM",
+    "user": "ARM",
+    "host": "90.189.132.25",  # ip - для разработки
+    "password": "ARM_SYSTEM"
+}
+
+SERVICE = {
+    'HOST': '0.0.0.0',
+    'PORT': 13451
+}
