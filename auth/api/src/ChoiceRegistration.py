8123a44af0c4dcfd68116adcee51f8aace55bb24
raldenprog <raldenprog@mail.ru>
2018-10-06 04:22:30 +0700

    * update user name

--- auth/api/src/ChoiceRegistration.py
+++ auth/api/src/ChoiceRegistration.py
@@ -1,11 +1,14 @@
 import auth.api.helpers.base_name as names
 from auth.api.src.CompanyRegistration import registration as CompReg
 from auth.api.src.UserRegistration import registration as UsReg
-from auth.api.src.UpdateUser import update
+from auth.api.src.UpdateUserAuth import update as uAuth
+from auth.api.src.UpdateUserName import update as uName
 
 def choice(user_data):
-    if user_data.get(names.ID_USER, None) is not None:
-        answer = update(user_data)
+    if user_data.get(names.NAME, None) is not None and user_data.get(names.ID_USER, None) is not None:
+        answer = uName(user_data)
+    elif user_data.get(names.ID_USER, None) is not None:
+        answer = uAuth(user_data)
     elif user_data.get(names.ID_COMPANY, None) is None:
         answer = CompReg(user_data)
     else:
