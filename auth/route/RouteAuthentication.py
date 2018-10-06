ca46bbc3e350b97a951a5afa8078a3a711321ae3
raldenprog <raldenprog@mail.ru>
2018-10-06 10:56:41 +0700

    * list operators

--- auth/route/RouteAuthentication.py
+++ auth/route/RouteAuthentication.py
@@ -3,13 +3,16 @@ from auth.api.helpers.service import Gis as gs
 from flask_restful import Resource, reqparse
 from auth.api.src.ChoiceRegistration import choice
 from auth.api.src.Authentication import auth
+from auth.api.src.Operators import listOperators
 
 class Authentication(Resource):
     def __init__(self):
         self.__parser = reqparse.RequestParser()
         self.__parser.add_argument('data')
+        self.__parser.add_argument('id_company')
         self.__args = self.__parser.parse_args()
         self.data = None
+        self.id_company = None
 
     def parse_data(self):
         self.data = self.__args.get('data', None)
@@ -23,6 +26,11 @@ class Authentication(Resource):
 
     def post(self):
         data = self.parse_data()
-        print(data)
         answer = auth(data)
+        return answer, 200, {'Access-Control-Allow-Origin': '*'}
+
+    def get(self):
+        self.id_company = self.__args.get('id_company', None)
+        print(self.id_company)
+        answer = listOperators(self.id_company)
         return answer, 200, {'Access-Control-Allow-Origin': '*'}
