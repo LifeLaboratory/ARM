06a44b8d032bc932e2dd141c4f74c36d5926a1cf
raldenprog <raldenprog@mail.ru>
2018-10-06 15:18:36 +0700

    * add type

--- auth/api/src/Authentication.py
+++ auth/api/src/Authentication.py
@@ -1,6 +1,7 @@
 import auth.api.helpers.base_name as names
 from auth.api.helpers.service import Sql
 import logging
+from auth.api.src.sessionToId import convert
 
 
 def auth(user_data):
@@ -24,9 +25,21 @@ def auth(user_data):
 def selectUser(user_data):
     try:
         session = Sql.exec(file="api/sql/select_user_auth.sql", args=user_data)
+        sesData = dict()
+        sesData[names.SESSION] = session[0]["session"]
+        condata = convert(sesData)
+        type = None
+        #print(condata)
+        if condata[names.DATA].get(names.ID_USER, None) is None and condata[names.DATA].get(names.ID_COMPANY,
+                                                                                            None) is not None:
+            type = "Company"
+        elif condata[names.DATA].get(names.ID_USER, None) is not None and condata[names.DATA].get(names.ID_COMPANY,
+                                                                                                  None) is not None:
+            type = "Operator"
+
     except:
         logging.error('error: Ошибка запроса к базе данных. Возможно такой пользователь уже есть')
         return {names.ANSWER: names.WARNING,
                 names.DATA: {"error_info":"Ошибка запроса к базе данных. Возможно такой пользователь уже есть"}}
-    return {names.ANSWER: names.SUCCESS, names.DATA: session[0]}
+    return {names.ANSWER: names.SUCCESS, names.DATA: session[0], names.TYPE: type}
 
