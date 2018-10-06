e301aac9d773a2c229606927c9a4c4f3df5b11f2
Anton Chusovitin <antonion2011@yandex.ru>
2018-10-06 12:39:32 +0700

    * добавлена структура чата

--- chat/api/src/fb/get_facebook_message.py
+++ chat/api/src/fb/get_facebook_message.py
@@ -1,11 +1,14 @@
 from fbchat import Client
 from fbchat.models import *
 
+
 def database_holding(event):
     return 1
 
+
 def send_to_operator(event):
     return 1
+
 
 def main():
     client = Client('den-isk1995@mail.ru', '2xkzWW+%nKND', max_tries=100)
@@ -17,8 +20,8 @@ def main():
 
 #            database_holding(unread_msg, msg)
 #            send_to_operator(unread_msg, msg)
-
     client.logout()
+
 
 if __name__ == '__main__':
     main()
--- chat/api/src/vk/get_vk_message.py
+++ chat/api/src/vk/get_vk_message.py
@@ -9,6 +9,7 @@ ACCESS_TOKEN = '08b030e59cfca185c1e95f0caba3e7f245ba47681ed31b36606c95fb8294e94b
 ACCESS_TOKEN_2 = 'b6b30f7014d5f65777496cbcc78874f9e7e7e11c0b276fea0f3cb77a68140a7ec492317472f801bb59d47'
 GROUP_ID = '172231442'
 
+
 def selectChat(user_data):
     try:
         session = Sql.exec(file="api/sql/chat/select_exists_chat.sql", args=user_data)
@@ -19,7 +20,11 @@ def selectChat(user_data):
     return {names.ANSWER: names.SUCCESS, names.DATA: session}
 
 
-# vk_api.bot_longpoll.VkBotMessageEvent'>({'type': 'message_new', 'object': {'date': 1538790046, 'from_id': 56176108, 'id': 19, 'out': 0, 'peer_id': 56176108, 'text': 'asdas', 'conversation_message_id': 18, 'fwd_messages': [], 'important': False, 'random_id': 0, 'attachments': [], 'is_hidden': False}, 'group_id': 172231442})>
+# vk_api.bot_longpoll.VkBotMessageEvent'>({'type': 'message_new', 'object':
+# {'date': 1538790046, 'from_id': 56176108, 'id': 19, 'out': 0, 'peer_id': 56176108,
+# 'text': 'asdas', 'conversation_message_id': 18, 'fwd_messages': [],
+# 'important': False, 'random_id': 0, 'attachments': [], 'is_hidden': False},
+# 'group_id': 172231442})>
 def database_holding(event):
     check = [names.ID_CHAT]
     chat_data = dict.fromkeys(check, '')
@@ -37,8 +42,10 @@ def database_holding(event):
         return 1
     return 1
 
+
 def send_to_operator(event):
     return 1
+
 
 def main():
     vk_session = vk_api.VkApi(token=ACCESS_TOKEN_2)     # Создаем сессию
@@ -46,9 +53,14 @@ def main():
 
     for event in longpoll.listen():                     # Ждем события
         if event.type == VkBotEventType.MESSAGE_NEW:    # Если приняли новое сообщение
-            # vk_api.bot_longpoll.VkBotMessageEvent'>({'type': 'message_new', 'object': {'date': 1538790046, 'from_id': 56176108, 'id': 19, 'out': 0, 'peer_id': 56176108, 'text': 'asdas', 'conversation_message_id': 18, 'fwd_messages': [], 'important': False, 'random_id': 0, 'attachments': [], 'is_hidden': False}, 'group_id': 172231442})>
+            # vk_api.bot_longpoll.VkBotMessageEvent'>({'type': 'message_new',
+            # 'object': {'date': 1538790046, 'from_id': 56176108, 'id': 19,
+            # 'out': 0, 'peer_id': 56176108, 'text': 'asdas', 'conversation_message_id': 18,
+            # 'fwd_messages': [], 'important': False, 'random_id': 0, 'attachments': [],
+            # 'is_hidden': False}, 'group_id': 172231442})>
             database_holding(event)                     # Работа с БД
             send_to_operator(event)
+
 
 if __name__ == '__main__':
     main()
