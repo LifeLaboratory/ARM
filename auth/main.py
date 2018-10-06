5fe5ad567d5ef7340ad34223c192464a8c597697
raldenprog <raldenprog@mail.ru>
2018-10-06 18:57:52 +0700

    * select list chat

--- auth/main.py
+++ auth/main.py
@@ -8,9 +8,9 @@ import logging
 from flask_restful import Api
 from auth.route.route_list import ROUTES
 
-logging.basicConfig(filename='logger.log',
-                        format='%(filename)-12s[LINE:%(lineno)d] %(levelname)-8s %(message)s %(asctime)s ',
-                        level=logging.DEBUG)
+#logging.basicConfig(filename='logger.log',
+#                        format='%(filename)-12s[LINE:%(lineno)d] %(levelname)-8s %(message)s %(asctime)s ',
+#                        level=logging.DEBUG)
 
 _app = flask.Flask(__name__)
 _app.config['JSON_AS_ASCII'] = False
--- chat/route/RouteChat.py
+++ chat/route/RouteChat.py
@@ -2,7 +2,7 @@
 from chat.api.helpers.service import Gis as gs
 from flask_restful import Resource, reqparse
 from chat.api.src.Chat import *
-
+from chat.api.helpers.sessionToId import convert
 
 class Classificator(Resource):
     def __init__(self):
@@ -12,6 +12,7 @@ class Classificator(Resource):
         self.__parser.add_argument('id_user')
         self.__parser.add_argument('id_client')
         self.__parser.add_argument('Message')
+        self.__parser.add_argument('Session')
         self.__args = self.__parser.parse_args()
         self.data = None
         self.id_company = None
@@ -22,18 +23,33 @@ class Classificator(Resource):
         self.data = gs.converter(self.data)
         return self.data
 
+    def selectid(self, data):
+        if data.get(names.SESSION, None) is not None:
+            condata = convert(data)
+            if condata[names.DATA].get(names.ID_USER, None) is None and condata[names.DATA].get(names.ID_COMPANY,
+                                                                                                None) is not None:
+                data[names.ID_COMPANY] = condata[names.DATA][names.ID_COMPANY]
+            elif condata[names.DATA].get(names.ID_USER, None) is not None and condata[names.DATA].get(names.ID_COMPANY,
+                                                                                                      None) is not None:
+                data[names.ID_COMPANY] = condata[names.DATA][names.ID_COMPANY]
+                data[names.ID_USER] = condata[names.DATA][names.ID_USER]
+            print("DATA", data)
+        return data
+
     def put(self):
         """
         Метод принимает добавление чата/сообщения
         :return:
         """
-        id_client = self.__args.get('id_client', None)
+        data = self.parse_data()
+        id_client = data.get('id_client', None)
         answer = insert_new_chat(id_client)
         return answer, 200, {'Access-Control-Allow-Origin': '*'}
 
     def post(self):
-        id_client = self.__args.get('id_client', None)
-        Message = self.__args.get('Message', None)
+        data = self.parse_data()
+        id_client = data.get('id_client', None)
+        Message = data.get('Message', None)
         answer = post_chat_message(id_client, Message)
         # auth(data)
         return answer, 200, {'Access-Control-Allow-Origin': '*'}
@@ -43,7 +59,15 @@ class Classificator(Resource):
         Метод получает списочные данные
         :return:
         """
-        self.id_user = self.__args.get('id_user', None)
-        print(self.id_user)
-        answer = get_chat_list_operator(self.id_user)
+        self.session = self.__args.get('Session', None)
+        print(self.session)
+        if self.session is not None:
+            data = dict()
+            data[names.SESSION] = self.session
+            condata = self.selectid(data)
+            print("CONDATA: ", condata)
+            answer = get_chat_list_operator(condata[names.ID_USER])
+            answer = gs.converter(answer)
+        else:
+            answer = {names.ANSWER: "Error", names.DATA: {"error_info": "Session not found"}}
         return answer, 200, {'Access-Control-Allow-Origin': '*'}
