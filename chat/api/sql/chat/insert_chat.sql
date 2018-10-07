af1951d2ea70fd0b376b3a6bd2cd2d5ced12ecf6
Anton Chusovitin <antonion2011@yandex.ru>
2018-10-07 15:32:24 +0700

    * фикс тайм зон

--- chat/api/sql/chat/insert_chat.sql
+++ chat/api/sql/chat/insert_chat.sql
@@ -7,5 +7,5 @@ min_us as (
      select "id_user", count(*) filter (where "status" = '0') as "min" from "chat_list"
     )
 insert into "chat_list"("id_company", "status", "date", "id_chat", "id_user")
-select 1, 'Open', now(), ('{SALT}'::text || '|' || {id_client}::text)::text, (select "id_user" from min_us order by "min" limit 1)
+select 1, 'Open', now()::timestamp + interval'7 h', ('{SALT}'::text || '|' || {id_client}::text)::text, (select "id_user" from min_us order by "min" limit 1)
 returning "id_chat"
--- chat/api/sql/chat/insert_message_chat.sql
+++ chat/api/sql/chat/insert_message_chat.sql
@@ -1,4 +1,4 @@
 insert into "chat_message"("id_chat", "message", "data", "sender")
 select ('{SALT}'::text || '|' || '{id_client}'::text)::text
-, '{Message}', '{Data_message}'::timestamp, '{Sender}'
+, '{Message}', '{Data_message}'::timestamp + interval'7 h', '{Sender}'
 returning "id_message"
